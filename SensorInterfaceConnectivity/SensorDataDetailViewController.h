159b132b7ba07bd6d8b71bc77de205fece4e239d
yukui_ye <yukui@getlatch.com>
2016-02-24 18:48:52 -0500

    first commit

---
+++ SensorInterfaceConnectivity/SensorDataDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SensorDataDetailViewController.h
+//  SensorInterfaceConnectivity
+//
+//  Created by Yukui Ye on 11/9/15.
+//  Copyright © 2015 Yukui Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorDataDetailViewController : UIViewController
+
+@end
---
+++ SensorInterfaceConnectivity/SensorDataDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  SensorDataDetailViewController.m
+//  SensorInterfaceConnectivity
+//
+//  Created by Yukui Ye on 11/9/15.
+//  Copyright © 2015 Yukui Ye. All rights reserved.
+//
+
+#import "SensorDataDetailViewController.h"
+
+@interface SensorDataDetailViewController ()
+
+@end
+
+@implementation SensorDataDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
